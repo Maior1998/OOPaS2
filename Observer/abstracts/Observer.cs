e3858cac428c569bf7c1a822bad76975ba1fc44e
Sergey Patoka <sergey.pat98@gmail.com>
2020-03-06 07:04:49 +0500

    Начало Observer

---
+++ Observer/abstracts/Observer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer.abstracts
+{
+    public abstract class Observer
+    {
+        public abstract void Update();
+
+    }
+}
---
+++ Observer/abstracts/StatsCenter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Observer.abstracts
+{
+    public abstract class Subject
+    {
+        private readonly List<Observer> _observers =new List<Observer>();
+
+        public void Register(Observer Target)
+        {
+            if(!_observers.Contains(Target))
+                _observers.Add(Target);
+        }
+
+        public void Remove(Observer Target)
+        {
+            _observers.Remove(Target);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (Observer Observer in _observers)
+                Observer.Update();
+        }
+    }
+}
